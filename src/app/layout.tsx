542318a235a01be0ad17cda3758ca39729251d5f
WOLFSGIT-24 <wolfsgit24@gmail.com>
2026-08-17 19:03:29 +0530

    ensure GTM script is first in head with async attr

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager - must be first in head */}
         <script
+          async
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5B4QDCTW');`,
           }}
@@ -18,6 +20,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="icon" href="/image-removebg-preview.png" type="image/png" />
       </head>
       <body>
+        {/* Google Tag Manager (noscript) - must be first in body */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-5B4QDCTW"
